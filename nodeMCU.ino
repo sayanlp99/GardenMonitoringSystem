7f7283ad1916bed6fcef0bb49f6ea6646abed178
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-12-31 11:58:29 +0530

    Update nodeMCU.ino

--- nodeMCU.ino
+++ nodeMCU.ino
@@ -1,18 +1,10 @@
 #include <ESP8266WiFi.h>
-#include <ArduinoJson.h>      //ArduinoJson v6.13.0
+#include <ArduinoJson.h>          //ArdunioJson v5.13.0
 const char* ssid     = "SSID";
 const char* password = "PASSWORD";
 WiFiServer server(80);
 String header;
-
-void jsonEncode(){
-  const size_t capacity = JSON_OBJECT_SIZE(3);
-  DynamicJsonDocument payload(capacity);
-  payload["rain"] = digitalRead(4);
-  payload["humidity"] = digitalRead(5);
-  payload["temperature"] = analogRead(A0);
-  serializeJson(payload, Serial);
-}
+int c=0;
 
 void setup() {
   Serial.begin(9600);
@@ -35,16 +27,22 @@ void setup() {
 void loop(){
   WiFiClient client = server.available();
   if (client) {
-            jsonEncode();                            
-            client.println("HTTP/1.1 200 OK");
-            client.println("Access-Control-Allow-Origin:*");
-            client.println("Content-type:application/json");
-            client.println("Server:NodeMCU");
-            client.println("Content-lenght:100");
-            client.println("Connection:keep-alive");
-            client.println();
-            client.println(payload);
-    Serial.println("Accessing");
+    const size_t capacity = JSON_OBJECT_SIZE(3);
+    DynamicJsonBuffer jsonBuffer(capacity);
+    JsonObject& root = jsonBuffer.createObject();
+    root["rain"] = digitalRead(4);
+    root["humidity"] = digitalRead(5);
+    root["temperature"] = analogRead(A0);                         
+    client.println("HTTP/1.1 200 OK");
+    client.println("Access-Control-Allow-Origin:*");
+    client.println("Content-type:application/json");
+    client.println("Server:NodeMCU");
+    client.println("Content-lenght:75");
+    client.println("Connection:keep-alive");
+    client.println();
+    root.printTo(client);
+    Serial.print("Accessing Data: ");
+    Serial.println(c++);
     delay(2000);
   }
 }
